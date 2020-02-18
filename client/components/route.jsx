dbb4472e1fbc0e95257ccd28104b357f5af0cfba
YunCYang <ericeorl2013@gmail.com>
2020-02-17 17:23:33 -0800

    added files for react structure

---
+++ client/components/route.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RRoute = () => {
+  return (
+    <div>
+      <h1>Route</h1>
+    </div>
+  );
+};
+
+export default RRoute;
---
+++ client/components/routeLog.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RouteLog = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default RouteLog;
