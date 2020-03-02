7e62ebd0e69968ccb077e59a48940cf9af94db5c
YunCYang <ericeorl2013@gmail.com>
2020-03-01 20:19:10 -0800

    added endpoints for stat page

--- client/components/stat.jsx
+++ client/components/stat.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Stat = () => {
   return (
-    <div>
-      <h1>Stat</h1>
+    <div className="section container py-2">
+      <div className='container'>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <span>Best Grade: </span>
+          </li>
+          <li className="list-group-item">
+            <span>Favorite Location: </span>
+          </li>
+          <li className="list-group-item">
+            <span>Average Attempts: </span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
